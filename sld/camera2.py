2a40a10ff742f64cba04f876c8b188cd5f800bf0
Guillaume Lauret <guillaumej.lauret@gmail.com>
2021-11-23 14:54:12 +0100

    updated notebook

--- sld/camera2.py
+++ sld/camera2.py
@@ -6,6 +6,19 @@ detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:
     success, img = cap.read()
-    hands, img = detector.findHands(img)
+    hands = detector.findHands(img) #With Draw
+    #hands, img = detector.findHands(img, flipType=True) #No Draw
+
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"] #List of 21 Landmarks points
+        bbox1 = hand1["bbox"] # Bounding Box info x, y, w, h
+        centerPoint1 = hand1["center"] # center of hand cx, cy
+        handType1 = hand1["type"] # Hand Type Left or Right
+
+        print(len(lmList1), lmList1)
+        #print(bbox1)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
