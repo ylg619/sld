6a6176f52fc36283755fb289ee0419e0456ee66c
Danny Cardoso <dcardoso066@gmail.com>
2021-11-26 17:23:09 +0100

    streamlite cam

--- sld/camera2.py
+++ sld/camera2.py
@@ -9,9 +9,7 @@ detector = HandDetector(detectionCon=0.8, maxHands=2)
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img) #With Draw
-    #hands, img = detector.findHands(img, flipType=True) #No Draw
-    copy = img.copy()
-    ROI_number = 0
+    #ROI_number = 0
 
 
     if hands:
@@ -20,11 +18,9 @@ while True:
         lmList1 = hand1["lmList"] #List of 21 Landmarks points
         bbox1 = hand1["bbox"] # Bounding Box info x, y, w, h
         x, y, w, h = bbox1
-        #centerPoint1 = hand1["center"] # center of hand cx, cy
         handType1 = hand1["type"] # Hand Type Left or Right
-        #print(len(lmList1), lmList1)
         ROI = img[y:y+h, x:x + w]
-        ROI_number += 1
+        print(ROI)
 
 
     cv2.imshow("Image", img)
