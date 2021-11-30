af5942562b05e9b43e189b943d1e3def4bfe474a
Guillaume Lauret <guillaumej.lauret@gmail.com>
2021-11-30 14:05:56 +0100

    update colors camera

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
 import queue
+import random
 # Import component
 from streamlit_webrtc import (
     RTCConfiguration,
@@ -17,13 +18,16 @@ from streamlit_webrtc import (
     WebRtcMode,
     webrtc_streamer,
 
-
 )
-
 #dictionary of traduction letters
 dict_letter = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'K', 10:'L', 11:'M',
                12:'N', 13:'O', 14:'P', 15:'Q', 16:'R', 17:'S', 18:'T', 19:'U', 20:'V', 21:'W', 22:'X', 23:'Y' }
-colors = np.random.uniform(0, 255, size=(len(dict_letter), 3))
+
+COLORS = np.random.uniform(0, 255, size=(len(dict_letter), 3))
+
+dict_colors = {}
+for i in range(24):
+    dict_colors[i] = COLORS[i]
 
 #Set up STUN servers
 RTC_CONFIGURATION = RTCConfiguration(
@@ -40,7 +44,6 @@ class SignPredictor(VideoProcessorBase):
 
     def __init__(self) -> None:
         # Hand detector
-
         self.hand_detector = HandDetector(detectionCon=0.8, maxHands=1)
         self.model = load_mo()
         self.counter = 0
@@ -58,11 +61,11 @@ class SignPredictor(VideoProcessorBase):
         if hands:
             bbox1 = hands[0]["bbox"]  # Bounding box info x,y,w,h
             x, y, w, h = bbox1
-            # rectangle = cv2.rectangle(image, (x - 20, y - 20),(x + w + 20, y + h + 20),(0, 255, 0), 2)
-            x_square = int(y - 0.2 * h)
-            y_square = int(x - 0.2 * w)
-            h_square = int(y + 1.2 * h)
+            x_square = int(x - 0.2 * w)
+            y_square = int(y - 0.2 * h)
             w_square = int(x + 1.2 * w)
+            h_square = int(y + 1.2 * h)
+
             if x_square < 0 :
                 x_square = 0
             if y_square < 0:
@@ -71,10 +74,10 @@ class SignPredictor(VideoProcessorBase):
                 h_square = 0
             if w_square < 0:
                 w_square = 0
-            hand_img = image_hand[x_square:h_square,y_square:w_square]  # image of the hand
+
+            hand_img = image_hand[y_square:h_square,x_square:w_square]  # image of the hand
             img_hand_resize = np.array(
-                tf.image.resize_with_pad(
-                    hand_img, 256, 256))  # resize image to match model's expected sizing
+                tf.image.resize_with_pad(hand_img, 256, 256))  # resize image to match model's expected sizing
             img_hand_resize = img_hand_resize.reshape(1, 256, 256, 3)
             img_hand_resize = tf.math.divide(img_hand_resize, 255)
             channels = tf.unstack(img_hand_resize, axis=-1)
@@ -82,16 +85,26 @@ class SignPredictor(VideoProcessorBase):
                                        axis=-1)
 
             prediction = self.model.predict(img_hand_resize)
-            #pickle.dump(prediction, 'predicts')
+            probabs = round(max(self.model.predict_proba(img_hand_resize)[0]),2)
             pred = np.argmax(prediction)
             self.counter +=1
             if self.counter % 1 == 0:
-                cv2.putText(image_hand, dict_letter[pred],
-                            (bbox1[0] + 50, bbox1[1] - 50),
-                            cv2.FONT_HERSHEY_PLAIN, 2, (57, 255, 20), 2)
+                cv2.putText(image_hand, f'{dict_letter[pred]}',
+                            (int(x_square + 1.5 * w), int(h_square - 0.7 * h)),
+                            cv2.FONT_HERSHEY_PLAIN, 2, dict_colors[pred], 2)
+                cv2.putText(image_hand, f'{str(probabs)}',
+                            (int(x_square + 1.5 * w), int(h_square - 0.5 * h)),
+                            cv2.FONT_HERSHEY_PLAIN, 2, dict_colors[pred], 2)
 
                 print(dict_letter[pred])
                 self.l.append(dict_letter[pred])
+
+                # COLORING BOX
+
+                cv2.rectangle(
+                    image_hand, (x_square, y_square),
+                    (w_square, h_square),
+                    (dict_colors[pred]), 2)
 
                 # QUEUE IN STREAMLIT:
 
@@ -141,11 +154,10 @@ if webrtc_ctx.state.playing:
                 result = webrtc_ctx.video_processor.result_queue_word.get(timeout=1.0)
                 final_word = ""
                 for value in result:
-                    final_word = final_word + value 
-                labels_placeholder.title(final_word)  
+                    final_word = final_word + value
+                labels_placeholder.title(final_word)
             except queue.Empty:
                 result = final_word
-            
+
         else:
             break
-
